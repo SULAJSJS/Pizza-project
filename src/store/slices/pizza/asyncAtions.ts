f2a5709e17373ed8daaa9910dfd26b23c1a7455a
SULAJSJS <chistotiktok@gmail.com>
2022-11-04 13:55:56 +0600

    Немного поправил API

--- src/store/slices/pizza/asyncAtions.ts
+++ src/store/slices/pizza/asyncAtions.ts
@@ -7,7 +7,7 @@ export const fetchPizzas = createAsyncThunk<IPizza[], SearchPizzaParams>(
   async (params) => {
     const { order, sortBy, category, search, currentPage } = params;
     const response = await axios.get<IPizza[]>(
-      `https://634670d6745bd0dbd37d9e6b.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      `https://63637b588a3337d9a2dedccc.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     );
     return response.data;
   },
